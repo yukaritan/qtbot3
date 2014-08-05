c75e660a770824806a3b97b50b2088c5e5890a8f
yukaritan <yukaritan@users.noreply.github.com>
2014-08-05 23:50:50 +0200

    added a little decorator that'll store who a user is every time they're seen - trying to find the source of an exception

--- qtbot3_service/util/handler_utils.py
+++ qtbot3_service/util/handler_utils.py
@@ -27,18 +27,13 @@ def msghook(regex):
 
 def remember_user(fn):
     """Remember a user"""
-    return fn
 
-    try:
-        def wrapper(message: Message, match, nick: str):
-            print("This feature is not implemented,"
-                  "but at some point in the future,"
-                  "the bot will remember who {nick} is".format(nick=message.nick))
-            return fn(message, match, nick)
-        return wrapper
-    except Exception as ex:
-        print("remember_user exception:", ex)
-    return fn
+    def wrapper(message: Message, match, nick: str):
+        print("This feature is not implemented,"
+              "but at some point in the future,"
+              "the bot will remember who {nick} is")
+        return fn(message, match, nick)
+    return wrapper
 
 
 def get_target(message: Message, nick: str) -> str:
