f046ed33cc45021b43bf95c65e76898c7fa06eb1
yukaritan <yukaritan@users.noreply.github.com>
2014-08-05 20:49:22 +0200

    added lua sandbox

--- qtbot3_service/plugins/lua.py
+++ qtbot3_service/plugins/lua.py
@@ -50,7 +50,7 @@ def join(message: Message, match, nick: str) -> str:
     print("received lua code from {nick}: {code}".format(nick=nick, code=code))
 
     try:
-        function = get_sandbox().eval('return ' + code)
+        function = get_sandbox().eval('function() return ' + code + ' end')
         result = '\r\n'.join(str(function()).splitlines())
     except Exception as ex:
         result = '\r\n'.join(str(ex).splitlines())
