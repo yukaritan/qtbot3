ffebd7040783edc8fd63f238cbdb40a652d45765
yukaritan <yukaritan@users.noreply.github.com>
2014-08-06 22:01:23 +0200

    - testing

--- qtbot3_service/plugins/test.py
+++ qtbot3_service/plugins/test.py
@@ -27,6 +27,9 @@ def get_host(message: Message, match, nick: str) -> str:
 
 
 @cmdhook('master')
+@cmdhook('mustard')
+@cmdhook('testings')
+@cmdhook('horses')
 def get_master(message: Message, match, nick: str) -> str:
     try:
         target = get_target(message, nick)
