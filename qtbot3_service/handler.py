fad2f5fdd7b77156f77868ae57c61703a3b98f05
yukaritan <yukaritan@users.noreply.github.com>
2014-08-06 21:08:07 +0200

    - messing with some test functionality - fixed a small bug

--- qtbot3_service/handler.py
+++ qtbot3_service/handler.py
@@ -1,5 +1,5 @@
 import util.irc as irc
-from util.handler_utils import hook, hooks, message_hooks, remember_user
+from util.handler_utils import hook, hooks, message_hooks, remember_user, get_master_nick
 from util.linetypes import LINE_TYPES
 from util.message import Message
 from util.settings import get_setting
