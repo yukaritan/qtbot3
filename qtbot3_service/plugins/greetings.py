05c6c01e4202599012fcfb017fd3f7983c9b303a
yukaritan <yukaritan@users.noreply.github.com>
2014-08-06 22:13:50 +0200

    - improved the greeting functionality slightly

--- qtbot3_service/plugins/greetings.py
+++ qtbot3_service/plugins/greetings.py
@@ -4,12 +4,23 @@ from util.handler_utils import msghook, get_target, hook, remember_user, ignore_
 from util.message import Message
 
 
+def prepare_greeting(nick: str) -> str:
+    greetings = ['Konnichiwa {nick}~ O-genki desu ka?',
+                 'Konnichiwa {nick}~',
+                 'Ohayō gozaimasu {nick}!',
+                 'Ohayō {nick}!',
+                 'Hi {nick}, how are you?',
+                 'Nice to see you {nick}. (^_^)']
+    greeting = random.choice(greetings)
+    return greeting.format(nick=nick)
+
+
 @hook('join')
 @remember_user
 @ignore_self
 def handle_join(message: Message, nick: str):
     print('join:', message.members)
-    return irc.chat_message(message.target, 'hai %s! ^__^' % message.nick)
+    return irc.chat_message(message.target, prepare_greeting(message.nick))
 
 
 @hook('part')
@@ -27,18 +38,9 @@ def handle_part(message: Message, nick: str):
 def greet(message: Message, match, nick: str) -> str:
     """nick is the bot's own nick"""
 
-    greetings = ['Konnichiwa {nick}~ O-genki desu ka?',
-                 'Konnichiwa {nick}~',
-                 'Ohayō gozaimasu {nick}!',
-                 'Ohayō {nick}!',
-                 'Hi {nick}, how are you?',
-                 'Nice to see you {nick}. (^_^)']
-
-    greeting = random.choice(greetings)
-
     lownick = nick.lower()
     lowmsg = message.message.lower()
     if lownick not in lowmsg and ('-' in nick and lownick.split('-', 1)[0] not in lowmsg):
         return
     target = get_target(message, nick)
-    return irc.chat_message(target, greeting.format(nick=message.nick))
+    return irc.chat_message(target, prepare_greeting(message.nick))
