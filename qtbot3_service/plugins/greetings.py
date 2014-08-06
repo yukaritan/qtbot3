b69421eea8b84111b8d3d9439dbd98065dcabc7a
yukaritan <yukaritan@users.noreply.github.com>
2014-08-06 23:32:03 +0200

    - small edit

--- qtbot3_service/plugins/greetings.py
+++ qtbot3_service/plugins/greetings.py
@@ -55,7 +55,6 @@ def greet(message: Message, match, nick: str) -> str:
 
     lownick = nick.lower()
     lowmsg = message.message.lower()
-    if lownick not in lowmsg and ('-' in nick and lownick.split('-', 1)[0] not in lowmsg):
-        return
-    target = get_target(message, nick)
-    return irc.chat_message(target, prepare_greeting(message.nick))
+    if lownick in lowmsg or ('-' in nick and lownick.split('-', 1)[0] in lowmsg):
+        target = get_target(message, nick)
+        return irc.chat_message(target, prepare_greeting(message.nick))
