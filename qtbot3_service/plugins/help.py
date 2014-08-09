da1fdabc14da04bd7b9ee9e5bde3096ad012604a
yukaritan <yukaritan@users.noreply.github.com>
2014-08-09 20:21:07 +0200

    adding a little help module

---
+++ qtbot3_service/plugins/help.py
@@ -0,0 +1,11 @@
+from util import irc
+from util.handler_utils import message_hooks, cmdhook, get_target
+from util.message import Message
+
+
+@cmdhook('help')
+def handle_help(message: Message, nick: str):
+    print('{nick} asked for help'.format(nick=message.nick))
+    response = ', '.join(message_hooks.keys())
+    target = get_target(message, nick)
+    return irc.chat_message(target, response)
