5ff58a8ebcf9f3c094f334b2f4c0c8af5fb9fe4d
yukaritan <yukaritan@users.noreply.github.com>
2014-08-10 19:41:25 +0200

    - adding rudimentary messaging

--- qtbot3_service/plugins/tell.py
+++ qtbot3_service/plugins/tell.py
@@ -1,17 +1,33 @@
-from util.handler_utils import msghook, store_value, fetch_all
+import json
+import time
+
+from util import irc
+from util.handler_utils import store_value, fetch_all, get_target, cmdhook
 from util.message import Message
 
 
-@msghook(';tell (?P<nick>[^\s]+) (?P<message>.+)')
+@cmdhook('tell (?P<recipient>[^\s]+) (?P<message>.+)')
 def handle_tell(message: Message, match, nick: str) -> str:
 
     # get the host for a given nick
     hosts = fetch_all(keyfilter='user_', valuefilter=match['nick'])
 
+    recipient = match['recipient']
+
+    target = get_target(message, nick)
+    if not hosts or recipient not in hosts.values():
+        msg = "found no users with the nick " + recipient
+        print(msg)
+        return irc.chat_message(target, msg)
+
     print("found hosts", hosts)
+    message = json.dumps([message.nick, match['message']])
 
+    for host in (k for k, v in hosts.items() if v == recipient):
+        key = "tell_" + host + str(time.time())
+        store_value(key, message)
 
-    #key = "tell_" + match['nick']
-
-
-    #store_value()
+#
+# @cmdhook('get told')
+# def handle_get_told(message: Message, match, nick: str) -> str:
+#
