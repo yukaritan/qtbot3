3c27eff142d9e7b033a48302f7f503fdada46ef4
yukaritan <yukaritan@users.noreply.github.com>
2014-08-10 12:05:17 +0200

    plugin loading on the fly

--- qtbot3_service/util/plugin_loader.py
+++ qtbot3_service/util/plugin_loader.py
@@ -1,3 +1,7 @@
+from util.handler_utils import cmdhook, authenticate
+from util.message import Message
+
+
 def load_plugin(name: str) -> bool:
     """name is the name of a file under qtbot3_service/plugins/, without the file extension"""
     name = name.replace('.', '')
@@ -10,3 +14,10 @@ def load_plugin(name: str) -> bool:
     except Exception as ex:
         print("failed to load plugin:", ex)
         return False
+
+
+@cmdhook('load_plugin (<?P<plugin>[^\s]+)')
+@authenticate
+def handle_loadplugin(message: Message, match, nick: str):
+    load_plugin(match['plugin'])
+
