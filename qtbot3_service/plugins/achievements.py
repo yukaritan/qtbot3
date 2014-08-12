e22c390f3983b0333bfaac2b81f5142f53aaea70
yukaritan <yukaritan@users.noreply.github.com>
2014-08-12 19:17:45 +0200

    - working on achievements

--- qtbot3_service/plugins/achievements.py
+++ qtbot3_service/plugins/achievements.py
@@ -8,8 +8,12 @@ from util.handler_utils import prehook, get_value, set_value
          ' (?P<target>[^\s]+)'
          '( :(?P<message>.*))?')
 def achievement_prehook(data: str, match: dict, nick: str):
-    key = 'chiev_partcount_' + match['host']
-    raw = get_value(key)
-    count = (json.loads(raw) + 1) if raw else 1
-    set_value(key, count)
-    print("Achievement progress for {host}: {count}".format(count=count, **match))
+    try:
+        key = 'chiev_partcount_' + match['host']
+        raw = get_value(key)
+        count = (json.loads(raw) + 1) if raw else 1
+        set_value(key, count)
+        print("Achievement progress for {host}: {count}".format(count=count, **match))
+
+    except Exception as ex:
+        print("achievement prehook exception:", ex)
