858c875cbb8919903da80421ccf6269434e8ba00
yukaritan <yukaritan@users.noreply.github.com>
2014-08-12 19:20:36 +0200

    - working on achievements

--- qtbot3_service/plugins/achievements.py
+++ qtbot3_service/plugins/achievements.py
@@ -10,7 +10,7 @@ from util.handler_utils import prehook, get_value, set_value
 def achievement_prehook(data: str, match: dict, nick: str):
     try:
         key = 'chiev_partcount_' + match['user']
-        count = get_value(key) or 1
+        count = (get_value(key) + 1) or 1
         set_value(key, count)
         print("Achievement progress for {user}: {count}".format(count=count, **match))
 
