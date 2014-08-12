73d54eae045c2c4e62bf7ed67d6d40d95ee695c7
yukaritan <yukaritan@users.noreply.github.com>
2014-08-12 19:02:13 +0200

    - working on achievements

--- qtbot3_service/plugins/achievements.py
+++ qtbot3_service/plugins/achievements.py
@@ -3,8 +3,8 @@ from util.handler_utils import prehook
 
 @prehook(':(?P<nick>[^\s]+)'
          '!(?P<user>[^\s]+)'
-         ' PRIVMSG'
+         ' PART'
          ' (?P<target>[^\s]+)'
-         ' :(?P<message>.+)')
+         '( :(?P<message>.*))?')
 def achievement_prehook(data: str, match: dict, nick: str):
     print("This got prehooked:", data, match)
