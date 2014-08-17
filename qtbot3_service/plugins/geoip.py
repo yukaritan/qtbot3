54306716bf42ed2ecfeae7bc3699005fc4076a60
yukaritan <yukaritan@users.noreply.github.com>
2014-08-17 18:49:39 +0200

    - improving geoip plugin

--- qtbot3_service/plugins/geoip.py
+++ qtbot3_service/plugins/geoip.py
@@ -4,6 +4,8 @@ from util import irc
 from util.handler_utils import cmdhook, fetch_all, get_target, get_value
 from util.message import Message
 
+from collections import OrderedDict
+
 
 @cmdhook('country (?P<nick>[^\s]+)')
 def handle_country_request(message: Message, match: dict, nick: str) -> str:
@@ -12,7 +14,8 @@ def handle_country_request(message: Message, match: dict, nick: str) -> str:
     fetched = fetch_all(keyfilter='user_', valuefilter=match['nick'])
     target = get_target(message, nick)
 
-    output = []
+    counter = OrderedDict(int)
+
     msg = "{nick} ({host}) is from {country}"
     for key in fetched:
         host = key.split('@', 1)[1]
@@ -25,12 +28,18 @@ def handle_country_request(message: Message, match: dict, nick: str) -> str:
             except:
                 country = geoip.country_name_by_name(host)
 
+            counter[country] += 1
+
             print("It resolves to", country)
-            output.append(irc.chat_message(target, msg.format(host=host, country=country, nick=match['nick'])))
         except Exception as e:
             print(e)
 
-    if not output:
+    if not counter:
         return irc.chat_message(target, "{nick} doesn't seem to be on earth".format(nick=match['nick']))
 
-    return output
+    results = sorted(counter.items(), key=lambda it: it[1])
+
+    message = "{nick}'s hosts resolve to the following countries: ".format(nick=match['nick'])
+    message += ', '.join("{0} {1} times".format(*result) for result in results)
+
+    return irc.chat_message(target, message)
